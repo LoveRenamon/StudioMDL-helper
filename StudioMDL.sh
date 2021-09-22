b3006eade6f6d08d144b1fc6f250e15f968e50a2
LoveRenamon <love_renamon@hotmail.com>
2021-09-22 15:01:41 -0300

    Added colors Beauty describe it you terminal also must be compatible with 256 colors (such urxvt)

--- StudioMDL.sh
+++ StudioMDL.sh
@@ -15,63 +15,96 @@ export GModbin="$SteamLibrary/GarrysMod/bin"
 
 export L4D2="Z:$SteamLibrary/Left 4 Dead 2/left4dead2"
 export L4D2bin="$SteamLibrary/Left 4 Dead 2/bin"
-export wine="/home/loverenamon/.steam/steam/steamapps/common/Proton 4.11/dist/bin/wine" # para l4d2 que tem DRM
+export proton4="/home/loverenamon/.steam/steam/steamapps/common/Proton 4.11/dist/bin/wine" # para l4d2 que tem DRM
 
 export GAME=$GMod # Opção default
 
 cd $SFMbin
-title="StudioMDL script para Source Engine. por: Davi (Debiddo) Gooz"
+title="\n\e[0;38;5;220mStudioMDL script para Source Engine. por: Davi (Debiddo) Gooz\e[0m\n"
 prompt="Selecione uma opção: jogo ou compilar: "
 options=("GMod" "SFM" "L4D2" "Compile")
 gamemsg="O jogo selecionado é agora é"
-compilemsg="Compilando em '$GAME/models' para o relativo jogo..."
+compilemsg="Compilando para o relativo jogo em:\n\e[1;49;97m\"$GAME/models\"\e[0m\n"
 
-echo "
-StudioMDL rodando em '$WINEARCH' dentro do prefixo '$WINEPREFIX'
-
+echo -e "\nStudioMDL rodando em \e[1;49;97m\"$WINEARCH\"\e[0m dentro do prefixo \e[1;49;97m\"$WINEPREFIX\"\e[0m\n
 
  Observações:
  	 - Use arquivos de texto em .qc
  	 - Não há necessidade de inserir a extensão do arquivo
  	 - Evite usar espaços nos diretórios
  	 - Insira o diretório relativo à:
-  $VPROJECT
+  \e[1;49;97m$VPROJECT0\e[0m
 
- Esse script roda em loop então poderá compilar inúmeros arquivos em sequência
-"
-echo "
-  $title
-"
+ Esse script roda em loop então poderá compilar inúmeros arquivos em sequência\n"
+echo -e "$title"
 
 PS3="$prompt"
 select opt in "${options[@]}" "Quit"; do
     case "$REPLY" in
-    1) echo "$gamemsg Garry's Mod"; export GAME=$GMod; cd $SFMbin;echo;;
-    g) echo "$gamemsg Garry's Mod"; export GAME=$GMod; cd $SFMbin;echo;;
-    gmod) echo "$gamemsg Garry's Mod"; export GAME=$GMod; cd $SFMbin;echo;;
+    1) echo "$gamemsg Garry's Mod"
+    export GAME=$GMod
+    cd $GModbin;echo;;
+    g) echo "$gamemsg Garry's Mod"
+    export GAME=$GMod
+    cd $GModbin;echo;;
+    gmod) echo "$gamemsg Garry's Mod"
+    export GAME=$GMod
+    cd $GModbin;echo;;
 
-    2) echo "$gamemsg Source Filmmaker"; export GAME=$SFM; cd $SFMbin;echo;;
-    s) echo "$gamemsg Source Filmmaker"; export GAME=$SFM; cd $SFMbin;echo;;
-    sfm) echo "$gamemsg Source Filmmaker"; export GAME=$SFM; cd $SFMbin;echo;;
+    2) echo "$gamemsg Source Filmmaker"
+    export GAME=$SFM
+    cd $SFMbin;echo;;
+    s) echo "$gamemsg Source Filmmaker"
+    export GAME=$SFM
+    cd $SFMbin;echo;;
+    sfm) echo "$gamemsg Source Filmmaker"
+    export GAME=$SFM
+    cd $SFMbin;echo;;
 
-    3) echo "$gamemsg Left 4 Dead 2"; export GAME="$L4D2"; cd "$L4D2bin";echo;;
-    l) echo "$gamemsg Left 4 Dead 2"; export GAME="$L4D2"; cd "$L4D2bin";echo;;
-    l4d2) echo "$gamemsg Left 4 Dead 2"; export GAME="$L4D2"; cd "$L4D2bin";echo;;
+    3) echo "$gamemsg Left 4 Dead 2"
+    export GAME="$L4D2"
+    cd "$L4D2bin";echo;;
+    l) echo "$gamemsg Left 4 Dead 2"
+    export GAME="$L4D2"
+    cd "$L4D2bin";echo;;
+    l4d2) echo "$gamemsg Left 4 Dead 2"
+    export GAME="$L4D2"
+    cd "$L4D2bin";echo;;
 
     # Compile part:
-    4) echo "$compilemsg"; read -p "Arquivo (.qc): " qcfile ; env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile";echo;echo $title;;
-    c) echo "$compilemsg"; read -p "Arquivo (.qc): " qcfile ; env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile";echo;echo $title;;
-    m) echo "$compilemsg"; read -p "Arquivo (.qc): " qcfile ; env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile";echo;echo $title;;
-    mdl) echo "$compilemsg"; read -p "Arquivo (.qc): " qcfile ; env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile";echo;echo $title;;
-    compile) echo "$compilemsg"; read -p "Arquivo (.qc): " qcfile ; env wine studiomdl $CompileFlags -game "$GAME" "V/0xdecompiled/:$qcfile";echo;echo $title;;
+    4) echo -e "$compilemsg"
+    read -p "QC: " qcfile
+    env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile"
+    echo -e $title;;
+    c) echo -e "$compilemsg"
+    read -p "QC: " qcfile
+    env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile"
+    echo -e $title;;
+    m) echo -e "$compilemsg"
+    read -p "QC: " qcfile
+    env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile"
+    echo -e $title;;
+    mdl) echo -e "$compilemsg"
+    read -p "QC: " qcfile
+    env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile"
+    echo -e $title;;
+    compile) echo -e "$compilemsg"
+    read -p "QC: " qcfile
+    env wine studiomdl $CompileFlags -game "$GAME" "V:/0xdecompiled/$qcfile"
+    echo -e $title;;
 
     # Especial para l4d2
-    lc) echo "Compilando especialmente em '$GAME/models' para Left 4 Dead 2 "; read -p "Arquivo (.qc): " qcfile ; cd "$L4D2bin";env WINEPREFIX="/mnt/500GB/SteamLibrary/steamapps/compatdata/563/pfx" "$wine" studiomdl -checklenghts -n -h -printbones -printgraph -nox360 -fastbuild -dumpmaterials -nop4 -verbose -game "$GAME" "V:/0xdecompiled/$qcfile";echo;echo $title;;
+    lc) echo -e "\nCompilando especialmente para \e[1;49;97mLeft 4 Dead 2\e[0m em:\n\e[1;49;97m\"$GAME/models\"\e[0m\n"
+    read -e -p "QC: " qcfile
+    cd "$L4D2bin"
+    env WINEPREFIX=/mnt/500GB/SteamLibrary/steamapps/compatdata/563/pfx "$proton4" ./studiomdl.exe '-checklenghts -n -h -printbones -printgraph -nox360 -fastbuild -dumpmaterials -nop4 -verbose' -game "$L4D2" "V:/0xdecompiled/$qcfile"
+    echo -e "\n$title\n";;
 
     $((${#options[@]}+1))) break;;
     quit) break;;
     sair) break;;
     q) break;;
-    *) echo "opção inválida. tente outra.";continue;;
+    *) echo -e "\e[0;38;5;160mOpção inválida. Tente outra.\e[0m\n"
+    continue;;
     esac
 done
